e6bde4e50c0b6f322c053e2fe28d14471c08d159
juanc <juanc@192.168.0.9>
2020-10-23 20:48:36 -0500

    Migración de métodos arbol AVL

--- src/avl/modelo/ArbolBinarioAVL.java
+++ src/avl/modelo/ArbolBinarioAVL.java
@@ -15,8 +15,24 @@ import java.util.logging.Logger;
  * @author carloaiza
  */
 public class ArbolBinarioAVL {
+    
+    int cant;
+    /**
+     * @return the altura
+     */
+    public int getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
 
     private NodoAVL raiz;
+     private int altura;
 
     //public void adicionarNodo()
     public NodoAVL getRaiz() {
@@ -85,6 +101,39 @@ public class ArbolBinarioAVL {
         }
 
     }
+    
+    //imprimir preorden
+    
+    public ArrayList preOrden() {
+        ArrayList l=new ArrayList();
+        preOrden(raiz,l);
+        return l;
+    }
+
+    private void preOrden(NodoAVL reco, ArrayList l) {
+        if (reco != null) {
+            l.add(reco.getDato() + " ");
+            preOrden(reco.getIzquierda(),l);
+            preOrden(reco.getDerecha(),l);
+        }
+    }
+    
+    
+      //imprimir postOrden
+    
+       public ArrayList postOrden() {
+        ArrayList l=new ArrayList();
+        postOrden(raiz,l);
+        return l;
+    }
+
+    private void postOrden(NodoAVL reco, ArrayList l) {
+        if (reco != null) {
+            postOrden(reco.getIzquierda(),l);
+            postOrden(reco.getDerecha(),l);
+            l.add(reco.getDato() + " ");
+        }
+    } 
 
     
 //
@@ -233,5 +282,359 @@ public class ArbolBinarioAVL {
 
     }
 
+ //Imprimir vista por niveles ordenado
+    
+     
+    public ArrayList imprimirNivel() {
+        ArrayList l=new ArrayList();
+        if (raiz != null)
+        {
+        String[] niveles = new String[raiz.obtenerAlturaNodo() + 1];        
+        imprimirNivel(raiz, 0, niveles);
+        for (int i = 0; i < niveles.length; i++) {
+            l.add(niveles[i] + " ");
+            System.out.println(niveles[i] + " ");
+        }
+        }
+        return l;
+    }
+      public void imprimirNivel(NodoAVL pivote, int nivel2, String[] niveles) {
+        if (pivote != null) {
+            niveles[nivel2] = pivote.getDato() + ", " + ((niveles[nivel2] != null) ? niveles[nivel2] : "");
+            imprimirNivel(pivote.getDerecha(), nivel2 + 1, niveles);
+            imprimirNivel(pivote.getIzquierda(), nivel2 + 1, niveles);
+        }
+    }
+    
+       //Imprimir arbol vista por hojas
+    public ArrayList getHojas() {
+        ArrayList l = new ArrayList();
+        getHojas(this.raiz, l);
+        return (l);
+    }
 
+    public void getHojas(NodoAVL r, ArrayList l) {
+        if (r != null) {
+            if (this.esHoja(r)) {
+                l.add(r.getDato());
+            }
+            getHojas(r.getIzquierda(), l);
+            getHojas(r.getDerecha(), l);
+        }
+
+    }
+    protected boolean esHoja(NodoAVL x) {
+        return (x != null && x.getIzquierda()== null && x.getDerecha()== null);
+    }
+    
+    
+        
+     //Imprimir buscar y buscar padre
+    
+    public boolean buscar(int x) {
+        return (buscar(this.raiz, x));
+
+    }
+    
+       private boolean buscar(NodoAVL r, int x) {
+        if (r == null) {
+            return (false);
+        }
+        int compara = ((Comparable) r.getDato()).compareTo(x);
+        if (compara > 0) {
+            return (buscar(r.getIzquierda(), x));
+        } else if (compara < 0) {
+            return (buscar(r.getDerecha(), x));
+        } else {
+            return (true);
+        }
+    }
+
+    public int padre(int info) {
+        if (info == 0 || this.raiz == null) {
+            return 0;
+        }
+        NodoAVL x = padre(this.raiz, info);
+        if (x == null) {
+            return 0;
+        }
+        return (x.getDato());
+    }
+
+    private NodoAVL padre(NodoAVL x, int info) {
+        if (x == null) {
+            return null;
+        }
+        if ((x.getIzquierda()!= null && x.getIzquierda().getDato()==(info)) || (x.getDerecha()!= null && x.getDerecha().getDato()==(info))) {
+            return (x);
+        }
+        NodoAVL y = padre(x.getIzquierda(), info);
+        if (y == null) {
+            return (padre(x.getDerecha(), info));
+        } else {
+            return (y);
+        }
+    } 
+    
+    //Método para borrar por niveles  
+    
+    
+    public void borrarNivel(int NivelDeseado) throws ArbolBinarioException, Exception {                
+                if (NivelDeseado > getAltura()){
+        ArrayList <Integer> nodosNivel=new ArrayList();
+        buscarNodosNivel(raiz, 1, nodosNivel, NivelDeseado);
+        for (int i = 0; i <nodosNivel.size(); i++) {
+            borrar(nodosNivel.get(i));        
+        }              
+      } 
+       else {
+                throw new ArbolBinarioException("No esta");
+            }
+                
+    }
+    
+    private void buscarNodosNivel(NodoAVL reco, int nivel,ArrayList nodos, int nivelDeseado) throws Exception{
+        if(nivel==nivelDeseado && reco!=null){
+            nodos.add(reco.getDato());
+        }
+        else if(raiz.obtenerAlturaNodo()<nivelDeseado){
+            throw new Exception();
+        }
+        else if(reco!=null){
+            buscarNodosNivel(reco.getIzquierda(), nivel+1, nodos, nivelDeseado);
+            buscarNodosNivel(reco.getDerecha(), nivel+1, nodos, nivelDeseado);
+            
+        }
+    }
+    //Método para eliminar hojas
+    
+     public void podar() {
+        podar(this.raiz);
+    }
+
+    private void podar(NodoAVL x) {
+        if (x == null) {
+            return;
+        }
+        if (this.esHoja(x.getIzquierda())) {
+            x.setIzquierda(null);
+        }
+        if (this.esHoja(x.getDerecha())) {
+            x.setDerecha(null);
+        }
+        podar(x.getIzquierda());
+        podar(x.getDerecha());
+        x.actualizarAltura();
+        balancear(x);
+    }
+    
+     //Método para borrar cualquier nodo
+    
+    public int borrar(int x) {
+        if (!this.buscar(x)) {
+            return 0;
+        }
+
+        NodoAVL z = borrar(this.raiz, x);
+        this.setRaiz(z);
+        return x;
+
+    }
+
+    private NodoAVL borrar(NodoAVL r, int x) {
+        if (r == null) {
+            return null;//<--Dato no encontrado		
+        }
+        int compara = ((Comparable) r.getDato()).compareTo(x);
+        if (compara > 0) {
+            r.setIzquierda(borrar(r.getIzquierda(), x));
+        } else if (compara < 0) {
+            r.setDerecha(borrar(r.getDerecha(), x));
+        } else {
+            System.out.println("Encontro el dato:" + x);
+            NodoAVL padre = padre(this.raiz, x);
+            if (r.getIzquierda()!= null && r.getDerecha()!= null) {
+                /*
+                 *	Buscar el menor de los derechos y lo intercambia por el dato
+                 *	que desea borrar. La idea del algoritmo es que el dato a borrar 
+                 *	se coloque en una hoja o en un nodo que no tenga una de sus ramas.
+                 **/
+                NodoAVL cambiar = buscarMin(r.getDerecha());
+                int aux = cambiar.getDato();
+                cambiar.setDato(r.getDato());
+                r.setDato(aux);
+                r.setDerecha(borrar(r.getDerecha(), x));
+                System.out.println("2 ramas");
+            } else {
+                r = (r.getIzquierda()!= null) ? r.getIzquierda(): r.getDerecha();
+                System.out.println("Entro cuando le faltan ramas ");
+            }
+        }
+        return r;
+    }
+    
+     // Método para buscar minimo
+    
+    private NodoAVL buscarMin(NodoAVL r) {
+        for (; r.getIzquierda()!= null; r = r.getIzquierda());
+        return (r);
+    }
+    
+    int numeroRamas = 0;
+    
+    public ArrayList<String>ObtenerRamaMayor(){
+    obtenernumeroRamas(this.raiz, 0);
+    return ObtenerRamamayor(this.raiz, 0, "", new ArrayList<String>());
+    }
+    private void obtenernumeroRamas(NodoAVL pivote, int contador) {
+        if (pivote != null) {
+            contador++;
+            obtenernumeroRamas(pivote.getIzquierda(), contador);
+            obtenernumeroRamas(pivote.getDerecha(), contador);
+        }
+        if (contador > this.numeroRamas) {
+            this.numeroRamas = contador;
+        }
+    }
+
+    private ArrayList<String> ObtenerRamamayor(NodoAVL pivote, int contador, String dato, ArrayList lista){
+        if (pivote != null ) {
+            dato+=pivote.getDato()+",";
+            contador ++;
+            lista=ObtenerRamamayor(pivote.getIzquierda(), contador, dato, lista);
+            lista=ObtenerRamamayor(pivote.getDerecha(), contador, dato, lista);
+            
+            if (contador == this.numeroRamas) {
+                lista.add(dato);
+            }
+        }
+        return lista;
+    }
+    // Método borrar mayor
+    
+    public String borrarMayor() {
+        NodoAVL reco=raiz.getIzquierda();
+        if (raiz != null) {
+            if (raiz.getDerecha()== null) {
+                raiz = raiz.getIzquierda();
+            } else {
+                NodoAVL anterior = raiz;
+                reco = raiz.getDerecha();
+                while (reco.getDerecha()!= null) {
+                    anterior = reco;
+                    reco = reco.getDerecha();
+                }
+                
+                anterior.setDerecha(reco.getIzquierda());
+                anterior.actualizarAltura();
+                balancear(anterior);
+            }
+        }
+        return ("Valor eliminado: " + reco.getDato());
+    }
+    
+    // Método Borrar menor
+    
+    public String borrarMenor() {
+        NodoAVL reco=raiz.getIzquierda();
+        if (raiz != null) {
+            if (raiz.getIzquierda()== null) {
+                raiz = raiz.getDerecha();
+            } else {
+                NodoAVL anterior = raiz;
+                reco = raiz.getIzquierda();
+                while (reco.getIzquierda()!= null) {
+                    anterior = reco;
+                    reco = reco.getIzquierda();
+                }
+                
+                anterior.setIzquierda(reco.getDerecha());
+                anterior.actualizarAltura();
+                balancear(anterior);
+            }
+        }
+        return ("Valor eliminado: " + reco.getDato());
+    }
+    
+    // Método cambiar valores
+    
+    public boolean cambiar() {
+            cambiar(raiz, 1);
+            //System.out.println();
+            return true;
+    }
+
+    private void cambiar(NodoAVL reco, int nivel) {
+        if (reco != null) {
+            reco.setDato(reco.getDato() * 3);
+            cambiar(reco.getIzquierda(), nivel + 1);
+            //System.out.print(reco.getDato() + " Nivel: (" + nivel + ") ,");
+            cambiar(reco.getDerecha(), nivel + 1);
+        }
+    }
+    
+    // Método cantidad de nodos del arbol
+    
+    public String cantidadNodos() {
+        cant = 0;
+        cantidadNodos(raiz);
+        return ""+cant;
+    }
+
+    private void cantidadNodos(NodoAVL reco) {
+        if (reco != null) {
+            cant++;
+            cantidadNodos(reco.getIzquierda());
+            cantidadNodos(reco.getDerecha());
+        }
+    }
+    
+    
+    // Método cantidad nodos hoja
+    
+    public String cantidadNodosHoja() {
+        cant = 0;
+        cantidadNodosHoja(raiz);
+        return ""+cant;
+    }
+      private void cantidadNodosHoja(NodoAVL reco) {
+        if (reco != null) {
+            if (reco.getIzquierda() == null && reco.getDerecha() == null) {
+                cant++;
+            }
+            cantidadNodosHoja(reco.getIzquierda());
+            cantidadNodosHoja(reco.getDerecha());
+        }
+    }
+    
+    // Método Menor Valor
+      
+      public String menorValor() throws ArbolBinarioException {
+          
+        String menor = "0";
+        if (raiz == null){
+            return menor;
+        }
+        else{
+             ArrayList <String> inOrden = inOrden();
+             menor = inOrden.get(0);
+       
+        return menor;
+        }       
+    }
+       
+     // Método Mayor Valor
+      
+      public String mayorValor() throws ArbolBinarioException {
+        String mayor = "0";
+        if (raiz == null){
+            return mayor;
+        }
+        else{
+             ArrayList <String> inOrden = inOrden();
+             mayor = inOrden.get(inOrden.size()-1);
+       
+        return mayor;
+        }
+    }
 }
